627824bedb7638b7f80dda0d2676f67a3f8d3f2b
Juan Antonio Domínguez <jadominguezarcas@gmail.com>
2024-12-15 13:28:31 +0100

    lint: pass flake8 linter

--- Python - 0 - Starting/ex02/find_ft_type.py
+++ Python - 0 - Starting/ex02/find_ft_type.py
@@ -1,16 +1,16 @@
 def all_thing_is_obj(object: any) -> int:
-	allowed_types = {
-		list: "List",
-		tuple: "Tuple",
-		set: "Set",
-		dict: "Dict",
-		str: "String",
-	}
+    allowed_types = {
+        list: "List",
+        tuple: "Tuple",
+        set: "Set",
+        dict: "Dict",
+        str: "String",
+    }
 
-	class_type = type(object)
-	prelude = allowed_types.get(class_type, "Type not found")
+    class_type = type(object)
+    prelude = allowed_types.get(class_type, "Type not found")
 
-	if class_type == str:
-		prelude = f"{object} is in the kitchen"
-	print(prelude if prelude == "Type not found" else f"{prelude} : {class_type}") 
-	return 42
+    if class_type == str:
+        prelude = f"{object} is in the kitchen"
+    print(prelude if prelude == "Type not found" else f"{prelude} : {class_type}")
+    return 42
