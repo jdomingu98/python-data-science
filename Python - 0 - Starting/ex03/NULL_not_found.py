006d2f459e188aeaa305969753d79172ff08af82
Juan Antonio Domínguez <jadominguezarcas@gmail.com>
2024-12-15 13:27:20 +0100

    fix: detect 0 as Zero and not Fake

--- Python - 0 - Starting/ex03/NULL_not_found.py
+++ Python - 0 - Starting/ex03/NULL_not_found.py
@@ -1,20 +1,24 @@
 import math
 
+
 def NULL_not_found(object: any) -> int:
-	allowed_types = {
-		None: "Nothing",
-		0: "Zero",
-		'': "Empty",
-		False: "Fake",
-	}
+    allowed_types = {
+        None: "Nothing",
+        '0': "Zero",
+        '': "Empty",
+        False: "Fake",
+    }
 
-	class_type = type(object)
-	prelude = allowed_types.get(object, "Type not found")
-	result = 1 if prelude == "Type not found" else 0
+    class_type = type(object)
+    if class_type == int:
+        object = str(object)
+    prelude = allowed_types.get(object, "Type not found")
+    result = 1 if prelude == "Type not found" else 0
 
-	if class_type == str and object:
-		prelude = "Type not found"
-	elif class_type == float and math.isnan(object):
-		prelude = "Cheese"
-	print(prelude if prelude == "Type not found" else f"{prelude}: {object} {class_type}")
-	return result
+    if class_type == str and object:
+        prelude = "Type not found"
+    elif class_type == float and math.isnan(object):
+        prelude = "Cheese"
+
+    print(prelude if prelude == "Type not found" else f"{prelude}: {object} {class_type}")
+    return result
